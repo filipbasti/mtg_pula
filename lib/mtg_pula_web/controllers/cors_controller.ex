875428c729dcf815cda5de37a73877b3e62872d1
filipbasti <filipbastijanic@gmail.com>
2025-02-28 12:40:50 +0100

    dodao link u CORS kontroler

--- lib/mtg_pula_web/controllers/cors_controller.ex
+++ lib/mtg_pula_web/controllers/cors_controller.ex
@@ -3,7 +3,7 @@ defmodule MtgPulaWeb.CORSController do
 
   def options(conn, _params) do
     conn
-    |> put_resp_header("access-control-allow-origin", "*") # Change * to your frontend URL in production
+    |> put_resp_header("access-control-allow-origin", "https://interface-mtgpula.onrender.com")
     |> put_resp_header("access-control-allow-methods", "GET, POST, PUT, DELETE, OPTIONS")
     |> put_resp_header("access-control-allow-headers", "Authorization, Content-Type, Accept, Origin, User-Agent, Cookie")
     |> send_resp(204, "")
