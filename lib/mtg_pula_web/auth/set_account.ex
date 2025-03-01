600a0b2eda078188f7887dea39219cff9bb34547
filipbasti <filipbastijanic@gmail.com>
2025-03-01 23:10:18 +0100

    debugging shit

--- lib/mtg_pula_web/auth/set_account.ex
+++ lib/mtg_pula_web/auth/set_account.ex
@@ -3,24 +3,31 @@ defmodule MtgPulaWeb.Auth.SetAccount do
   alias MtgPulaWeb.Auth.ErrorResponse
   alias MtgPula.Accounts
   require Logger
-  def init(_options)do
 
+  def init(_options) do
   end
 
-  def call(conn, _options)do
-    if conn.assigns[:account]do
+  def call(conn, _options) do
+    if conn.assigns[:account] do
+      Logger.debug("Account already assigned: #{inspect(conn.assigns[:account])}")
       conn
     else
       account_id = get_session(conn, :account_id)
-      Logger.debug("Account id from session: #{inspect(account_id)}")
-      if account_id == nil, do: raise ErrorResponse.Unauthorized
+      Logger.debug("Account ID from session: #{inspect(account_id)}")
+      if account_id == nil do
+        Logger.debug("Account ID is nil, raising Unauthorized error")
+        raise ErrorResponse.Unauthorized
+      end
       account = Accounts.get_full_account(account_id)
+      Logger.debug("Fetched account: #{inspect(account)}")
       cond do
-        account_id && account -> assign(conn, :account, account)
-        true -> assign(conn, :account, nil)
+        account_id && account ->
+          Logger.debug("Assigning account to conn: #{inspect(account)}")
+          assign(conn, :account, account)
+        true ->
+          Logger.debug("Assigning nil to conn account")
+          assign(conn, :account, nil)
       end
     end
-
-
   end
 end
